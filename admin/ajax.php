5d81cf34fdb28f2c5fb231fda7aeebbbd184ae62
sasan zare <sasanzare75@gmail.com>
2020-01-14 14:13:32 +0330

    feat : Features
    login

--- admin/ajax.php
+++ admin/ajax.php
@@ -1,1 +1,34 @@
- 
+<?php
+   if (file_exists('../process/core.php')) {
+       require_once('../process/core.php');
+   } else {
+       die('Common File is Missing !');
+   }
+   if (!isset($_POST["a"]))  {
+       $called_action = '';
+   } else {
+       $called_action = $_POST["a"];
+   }
+switch ($called_action) {
+   case "alogin":
+##-----A Login-----
+       if (!isset($_POST["fadmin"])) {
+           die("No Username !");
+       } else  {
+           $fadmin = $_POST["fadmin"];
+       }
+       if (!isset($_POST["fpass"])) {
+           die("No Password !");
+       } else  {
+           $fpass = $_POST["fpass"];
+       }
+       LoginUser($fadmin,$fpass);
+   break;
+   case "logout":
+##-----Logout-----
+       LogoutUser();
+   break;
+   default:
+       echo " ";
+   }
+?>
